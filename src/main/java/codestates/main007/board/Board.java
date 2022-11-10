e63914f35fce67e9512a7593c2c246407e546047
casava840 <hen3403@gmail.com>
2022-11-10 19:37:27 +0900

    Test: Board-tags제외한 값

---
+++ src/main/java/codestates/main007/board/Board.java
@@ -0,0 +1,30 @@
+package codestates.main007.board;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String review;
+    private float star;
+    private String thumbNail;
+    private double latitude;
+    private double longitude;
+    private String station;
+    private String category;
+    //private List<Tag> tags;
+    private String address;
+}
