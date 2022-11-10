c024b6bff71ee775be09b27b7489c3d08fcf2ead
casava840 <hen3403@gmail.com>
2022-11-10 20:56:18 +0900

    Test: getBoardTest 추가

--- src/test/java/codestates/main007/board/BoardControllerRestDocsTest.java
+++ src/test/java/codestates/main007/board/BoardControllerRestDocsTest.java
@@ -23,6 +23,8 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BoardController.class)
@@ -77,6 +79,49 @@ public class BoardControllerRestDocsTest {
                                         fieldWithPath("timeFromStation").type(JsonFieldType.STRING).description("역에서 부터 거리(분)"),
                                         fieldWithPath("dibs").type(JsonFieldType.BOOLEAN).description("게시글 찜 여부"),
                                         fieldWithPath("createdAt").type(JsonFieldType.STRING).description("게시글 생성 시간")
+                                )
+                        )
+                ));
+    }
+    @Test
+    @DisplayName("board 조회 테스트")
+    public void getBoardTest() throws Exception {
+        long id = 1;
+        ResultActions actions =
+                mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/boards/{board-Id}", id)
+                                .param("stationId", String.valueOf(1))
+                                .header("Authorization", "accessToken")
+                                .accept(APPLICATION_JSON)
+                );
+        actions
+                .andExpect(status().isOk())
+                .andDo(document(
+                        "get-board",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("board-Id").description("멤버 식별자 ID")
+                        ),
+                        requestHeaders(
+                                headerWithName("Authorization").description("사용자 인증 정보")
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시글 식별자 ID").ignored(),
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("게시글 본문 내용"),
+                                        fieldWithPath("review").type(JsonFieldType.STRING).description("게시글 본문 내용"),
+                                        fieldWithPath("star").type(JsonFieldType.NUMBER).description("게시글 별점 정보"),
+                                        fieldWithPath("thumbNail").type(JsonFieldType.STRING).description("게시글 썸네일"),
+                                        fieldWithPath("station").type(JsonFieldType.STRING).description("근처 기차역"),
+                                        fieldWithPath("category").type(JsonFieldType.STRING).description("게시글 카테고리"),
+                                        fieldWithPath("tags").type(JsonFieldType.ARRAY).description("게시글 태그들"),
+                                        fieldWithPath("tags[].tag.id").type(JsonFieldType.NUMBER).description("게시글 태그 ID"),
+                                        fieldWithPath("tags[].tag.tagString").type(JsonFieldType.STRING).description("게시글 태그이름"),
+                                        fieldWithPath("address").type(JsonFieldType.STRING).description("도로명주소"),
+                                        fieldWithPath("timeFromStation").type(JsonFieldType.STRING).description("역에서 부터 거리(분)"),
+                                        fieldWithPath("dibs").type(JsonFieldType.BOOLEAN).description("게시글 찜 여부"),
+                                        fieldWithPath("createdAt").type(JsonFieldType.STRING).description("게시글 생성시간")
                                 )
                         )
                 ));
