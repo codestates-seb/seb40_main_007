bb147d83adb0ee9f28aef1f23c49e974b50cf17d
leejehoon92 <2eejehoon@gmail.com>
2022-11-10 17:31:57 +0900

    style: 공통 헤더 간격 조정

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <header className="w-screen h-[44px] flex flex-row justify-between border-b-[6px] border-b-[rgb(83,199,240)] py-1 px-2">
+      <header className="w-screen h-[50px] flex flex-row justify-between border-b-[6px] border-b-[rgb(83,199,240)] p-2">
         <div className="flex flex-row">
           <img src="/images/bluelogo.png" alt="logo" />
           <span className="text-xl text-[rgb(83,199,240)] ml-2">역이요</span>
