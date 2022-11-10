4497d721e31ef02b5b9a8a1e5ca8012aef79a0b1
leejehoon92 <2eejehoon@gmail.com>
2022-11-10 17:04:34 +0900

    style: 공통 헤더 제작

--- client/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = () => {
-  return (
-    <>
-      <button className="btn btn-hover">확인</button>
-    </>
-  );
-};
-
-export default Button;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
     <>
-      <header className="w-screen h-12 flex flex-row align-middle">
-        <div>
-          <img src="..." alt="logo" />
-          <span className="">역이요</span>
+      <header className="w-screen h-[44px] flex flex-row justify-between border-b-[6px] border-b-[rgb(83,199,240)] py-1 px-2">
+        <div className="flex flex-row">
+          <img src="/images/bluelogo.png" alt="logo" />
+          <span className="text-xl text-[rgb(83,199,240)] ml-2">역이요</span>
         </div>
         <div>
           <button className="btn">LOGIN</button>
