9268c28614beb7ceb422da4314cb86961d23b621
woong-sung <cjswo138@gmail.com>
2022-11-11 21:26:07 +0900

    feat: pathComment, deleteComment 생성

--- src/main/java/codestates/main007/comments/CommentController.java
+++ src/main/java/codestates/main007/comments/CommentController.java
@@ -14,11 +14,30 @@ public class CommentController {
     @PostMapping("/boards/{board-id}/comments")
     @ResponseStatus(HttpStatus.CREATED)
     public void postComment(@RequestHeader(name = "Authorization")String accessToken,
+                          @PathVariable("board-id") long boardID,
                           @RequestBody CommentDto.Input postDto){
+        // todo: boardID 와 연관관계 설정
         Comment comment = this.commentMapper.commentDtoToComment(postDto);
 
         this.commentService.save(comment);
     }
 
+    @PatchMapping("/comments/{comment-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void patchComment(@RequestHeader(name = "Authorization")String accessToken,
+                            @PathVariable("comment-id") long commentId,
+                            @RequestBody CommentDto.Input patchDto){
+        // todo: boardID 와 연관관계 설정
+        this.commentService.update(patchDto, commentId);
+    }
+
+    @DeleteMapping("/comments/{comment-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComment(@RequestHeader(name = "Authorization")String accessToken,
+                             @PathVariable("comment-id") long commentId
+                             ){
+        // todo: boardID 와 연관관계 설정
+        this.commentService.delete(commentId);
+    }
 
 }
--- src/main/java/codestates/main007/comments/CommentService.java
+++ src/main/java/codestates/main007/comments/CommentService.java
@@ -8,7 +8,22 @@ import org.springframework.stereotype.Service;
 public class CommentService {
     private final CommentRepository commentRepository;
 
-    public void save(Comment comment){
+    public void save(Comment comment) {
         this.commentRepository.save(comment);
     }
+
+    public void update(CommentDto.Input patchDto, long commentId) {
+        Comment comment = find(commentId);
+        comment.patchComment(patchDto.getComment());
+    }
+
+    public Comment find(long commentId) {
+        return this.commentRepository.findById(commentId)
+                .orElseThrow(() -> new NullPointerException("해당 댓글이 존재하지 않습니다."));
+    }
+
+    public void delete(long commentId){
+        this.commentRepository.deleteById(commentId);
+    }
 }
+
