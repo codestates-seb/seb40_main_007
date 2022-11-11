63ad7cffaa028499dac2e687e045be648e2b9502
woong-sung <cjswo138@gmail.com>
2022-11-11 21:53:51 +0900

    feat: mypageDto 생성

---
+++ src/main/java/codestates/main007/member/MemberDto.java
@@ -0,0 +1,20 @@
+package codestates.main007.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MemberDto {
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyPage{
+        private long boardId;
+        private String title;
+        private String review;
+        private double star;
+        private int timeForStation;
+    }
+}
