fbd0e05863433b88ca25344b82fba230670fb1bb
woong-sung <cjswo138@gmail.com>
2022-11-11 20:45:49 +0900

    feat: 아직 작성 불가능한 메서드 틀만 작성

--- src/main/java/codestates/main007/board/BoardController.java
+++ src/main/java/codestates/main007/board/BoardController.java
@@ -12,40 +12,56 @@ public class BoardController {
     private final BoardMapper boardMapper;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-//    public void postBoard(@RequestHeader(name = "Authorization")String accessToken,
-//                          @RequestBody BoardDto.Post postDto){
-    public void postBoard(@RequestBody BoardDto.Input postDto){
+    public void postBoard(@RequestHeader(name = "Authorization")String accessToken,
+                          @RequestBody BoardDto.Input postDto){
         Board board = this.boardMapper.boardDtoToBoard(postDto);
         this.boardService.save(board);
     }
 
     @PatchMapping("/{board-id}")
     @ResponseStatus(HttpStatus.OK)
-//    public void patchBoard(@RequestHeader(name = "Authorization")String accessToken,
-//                          @PathVariable("board-id") long boardId ,
-//                          @RequestBody BoardDto.Post postDto){
-    public void patchBoard(@PathVariable("board-id") long boardId ,
-                           @RequestBody BoardDto.Input patchDto){
+    public void patchBoard(@RequestHeader(name = "Authorization")String accessToken,
+                          @PathVariable("board-id") long boardId ,
+                          @RequestBody BoardDto.Input patchDto){
         this.boardService.update(boardId, patchDto);
     }
 
     @DeleteMapping("/{board-id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void deleteBoard(@RequestHeader(name = "Authorization")String accessToken,
-//            @PathVariable("board-id") long boardId){
-    public void deleteBoard(@PathVariable("board-id") long boardId){
+    public void deleteBoard(@RequestHeader(name = "Authorization")String accessToken,
+            @PathVariable("board-id") long boardId){
         this.boardService.delete(boardId);
     }
 
     @GetMapping("/{board-id}")
     @ResponseStatus(HttpStatus.OK)
-//        public void getBoard(@RequestHeader(name = "Authorization")String accessToken,
-//            @PathVariable("board-id") long boardId){
-    public BoardDto.DetailResponse getBoard(@PathVariable("board-id") long boardId){
+        public BoardDto.DetailResponse getBoard(@RequestHeader(name = "Authorization")String accessToken,
+            @PathVariable("board-id") long boardId){
         Board board = this.boardService.find(boardId);
 
         BoardDto.DetailResponse detailResponse = this.boardMapper.boardToDetailResponseDto(board);
 
         return detailResponse;
+    }
+
+    @PostMapping("{board-id}/up-vote")
+    @ResponseStatus(HttpStatus.OK)
+    public void upVote(@RequestHeader(name = "Authorization")String accessToken,
+                       @PathVariable("board-id") long boardId){
+        // todo: 추천 기능
+    }
+
+    @PostMapping("{board-id}/down-vote")
+    @ResponseStatus(HttpStatus.OK)
+    public void downVote(@RequestHeader(name = "Authorization")String accessToken,
+                       @PathVariable("board-id") long boardId){
+        // todo : 비추천 기능
+    }
+
+    @PostMapping("{board-id}/dibs")
+    @ResponseStatus(HttpStatus.OK)
+    public void dibs(@RequestHeader(name = "Authorization")String accessToken,
+                         @PathVariable("board-id") long boardId){
+        //todo: 접속한 유저가 해당 게시글의 찜 눌렀는지 여부 반환
     }
 }
