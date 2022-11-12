2f9977913a87ac238248164973240ded230c7128
woong-sung <cjswo138@gmail.com>
2022-11-12 20:21:54 +0900

    refactor: 코드 정렬

--- src/main/java/codestates/main007/service/EmailService.java
+++ src/main/java/codestates/main007/service/EmailService.java
@@ -5,13 +5,14 @@ import org.springframework.mail.javamail.JavaMailSender;
 
 public class EmailService {
     private JavaMailSender mailSender;
-    public void sendMail(String email){
+
+    public void sendMail(String email) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("yeogiyo007@gmail.com");
         message.setTo(email);
         message.setSubject("안녕하세요 역이요입니다.");
         //todo: 패스워드를 찾아 일부분을 ●로 바꾸는 메서드
-        message.setText("귀하의 비밀번호는 "+"입니다.");
+        message.setText("귀하의 비밀번호는 " + "입니다.");
         mailSender.send(message);
     }
 }
--- src/main/java/codestates/main007/service/RandomAvatarService.java
+++ src/main/java/codestates/main007/service/RandomAvatarService.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomAvatarService {
-    public String genAvatar(){
+    public String genAvatar() {
         List<String> images = new ArrayList<>();
         images.add("https://pre-032-bucket.s3.ap-northeast-2.amazonaws.com/profile_blue.png");
         images.add("https://pre-032-bucket.s3.ap-northeast-2.amazonaws.com/profile_red.png");
