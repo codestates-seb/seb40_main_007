832f59ea4b3d792b56a3677e100bf7f64027f109
casava840 <hen3403@gmail.com>
2022-11-13 00:10:51 +0900

    Feat: SecurityConfiguration 추가

--- src/main/java/codestates/main007/config/SecurityConfiguration.java
+++ src/main/java/codestates/main007/config/SecurityConfiguration.java
@@ -1,9 +1,20 @@
 package codestates.main007.config;
 
+import codestates.main007.auth.filter.JwtAuthenticationFilter;
+import codestates.main007.auth.filter.JwtVerificationFilter;
+import codestates.main007.auth.handler.MemberAuthenticationEntryPoint;
+import codestates.main007.auth.handler.MemberAccessDeniedHandler;
+import codestates.main007.auth.handler.MemberAuthenticationFailureHandler;
+import codestates.main007.auth.handler.MemberAuthenticationSuccessHandler;
+import codestates.main007.auth.jwt.JwtTokenizer;
+import codestates.main007.auth.util.CustomAuthorityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,21 +25,40 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 public class SecurityConfiguration {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable()//csrf 일단 비활성화
-            .cors(Customizer.withDefaults())
-            .formLogin().disable()
-            .httpBasic().disable()
-            .authorizeHttpRequests(authorize -> authorize
-                    .antMatchers(POST, "/auth/login").permitAll()
-            );
-    return http.build();
+        http.csrf().disable()//csrf 일단 비활성화
+                .cors(Customizer.withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
+                .accessDeniedHandler(new MemberAccessDeniedHandler())
+                .and()
+                .apply(new CustomFilterConfigurer())
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers(POST, "/login").permitAll()
+                        .antMatchers(GET, "/{station-id}").permitAll()
+                        //todo:계속 추가예정
+                );
+        return http.build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -45,5 +75,23 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+            jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler());
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
+
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+        }
     }
 }
