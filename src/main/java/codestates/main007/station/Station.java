27374beb979653a31b662788e363ed186f967ae7
woong-sung <cjswo138@gmail.com>
2022-11-13 21:22:39 +0900

    feat : 모든 역 정보 추가

---
+++ src/main/java/codestates/main007/station/Station.java
@@ -0,0 +1,142 @@
+package codestates.main007.station;
+
+import lombok.Getter;
+
+public class Station {
+    @Getter
+    public static class Haengsin{
+        long stationId = 1;
+        String name = "행신역";
+        double latitude = 37.6121307;
+        double longitude = 126.8341891;
+    }
+
+    @Getter
+    public static class Seoul{
+        long stationId = 2;
+        String name = "서울역";
+        double latitude = 37.5549890;
+        double longitude = 126.9695548;
+    }
+
+    @Getter
+    public static class Yeongdeungpo{
+        long stationId = 3;
+        String name = "영등포역";
+        double latitude = 37.5156680;
+        double longitude = 126.9076660;
+    }
+
+    @Getter
+    public static class Gwangmyeong{
+        long stationId = 4;
+        String name = "광명역";
+        double latitude = 37.4163636;
+        double longitude = 126.8848772;
+    }
+
+    @Getter
+    public static class Suwon{
+        long stationId = 5;
+        String name = "수원역";
+        double latitude = 37.2656796;
+        double longitude = 127.0002404;
+    }
+
+    @Getter
+    public static class CheonanAsan{
+        long stationId = 6;
+        String name = "천안아산역";
+        double latitude = 36.7945269;
+        double longitude = 127.1044505;
+    }
+
+    @Getter
+    public static class Osong{
+        long stationId = 7;
+        String name = "오송역";
+        double latitude = 36.6204748;
+        double longitude = 127.3271435;
+    }
+
+    @Getter
+    public static class Daejeon{
+        long stationId = 8;
+        String name = "대전역";
+        double latitude = 36.3313335;
+        double longitude = 127.4330053;
+    }
+
+    @Getter
+    public static class Gimcheon{
+        long stationId = 9;
+        String name = "김천구미역";
+        double latitude = 36.1135294;
+        double longitude = 128.1811768;
+    }
+
+    @Getter
+    public static class Seodaegu{
+        long stationId = 10;
+        String name = "서대구역";
+        double latitude = 35.8813532;
+        double longitude = 128.5398916;
+    }
+
+    @Getter
+    public static class Dongdaegu{
+        long stationId = 11;
+        String name = "동대구역";
+        double latitude = 35.8794362;
+        double longitude = 128.6287756;
+    }
+
+    @Getter
+    public static class Milyang{
+        long stationId = 12;
+        String name = "밀양역";
+        double latitude = 35.4744720;
+        double longitude = 128.7711620;
+    }
+
+    @Getter
+    public static class Gupo{
+        long stationId = 13;
+        String name = "구포역";
+        double latitude = 35.2054724;
+        double longitude = 128.9971420;
+    }
+
+    @Getter
+    public static class Busan{
+        long stationId = 14;
+        String name = "부산역";
+        double latitude = 35.1177214;
+        double longitude = 129.0449881;
+    }
+
+    @Getter
+    public static class Ulsan{
+        long stationId = 15;
+        String name = "울산역";
+        double latitude = 35.5514992;
+        double longitude = 129.1386316;
+    }
+
+    @Getter
+    public static class Singyeongju{
+        long stationId = 16;
+        String name = "신경주역";
+        double latitude = 35.7980811;
+        double longitude = 129.1393977;
+    }
+    @Getter
+    public static class Pohang{
+        long stationId = 17;
+        String name = "포항역";
+        double latitude = 36.0716843;
+        double longitude = 129.3419644;
+    }
+
+
+}
