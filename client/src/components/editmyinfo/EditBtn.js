913bcd56e312fef98045e7995d57f3097293af05
raon9401 <sion9401@gmail.com>
2022-11-14 00:33:35 +0900

    [#41]Style: 내 정보 수정

---
+++ client/src/components/editmyinfo/EditBtn.js
@@ -0,0 +1,28 @@
+import { TiPencil } from "react-icons/ti";
+
+const EditBtn = ({ usePlace, iconSize, position }) => {
+  return (
+    <button
+      className={`border-[rgb(83,199,240)] rounded-full border-[2px]
+      }] p-[1px] m-[3px] ${position ? position : "static"}
+      ${usePlace === "main" ? "bg-[rgb(83,199,240)]" : "bg-white"}`}
+    >
+      <TiPencil
+        className="relative"
+        color={usePlace === "main" ? "white" : "rgb(83,199,240)"}
+        size={iconSize}
+      ></TiPencil>
+    </button>
+  );
+};
+
+/*
+    usePlace(type : string) : "EditProfileImg" 과 "main" 의 경우로 색상 변경됩니다.
+    iconSize : 아이콘 크기를 변경할 수 있습니다. 설정 안하면 기본 크기.
+    position(type : string) : position 속성 ("static", "fixed", "absolute", "relative", "sticky")
+                              설정 안하면 기본 설정인 "static"
+
+    ex) <EditBtn usePlace={"main"} iconSize={20} position="static"/>
+*/
+
+export default EditBtn;
