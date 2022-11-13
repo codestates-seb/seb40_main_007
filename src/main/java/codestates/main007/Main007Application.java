3af64643e2d5c7c0fad41e8eec1c90bc7013f06c
woong-sung <cjswo138@gmail.com>
2022-11-13 14:39:03 +0900

    feat: async 어노테이션으로 이메일 전송과정을 비동기로 전환

--- src/main/java/codestates/main007/Main007Application.java
+++ src/main/java/codestates/main007/Main007Application.java
@@ -2,8 +2,10 @@ package codestates.main007;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Main007Application {
 
 	public static void main(String[] args) {
