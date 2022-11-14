de734558984d7ba027ffd04067437fe46aae89db
raon9401 <sion9401@gmail.com>
2022-11-15 00:19:24 +0900

    fix: Header 로고 이미지 이름 변경

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <>
       <header className="w-screen h-[50px] flex flex-row justify-between border-b-[2px] border-b-[rgb(83,199,240)] p-2 fixed z-10 bg-white">
         <div className="flex flex-row">
-          <img src="/images/bluelogo.png" alt="logo" />
+          <img src="/images/logo.png" alt="logo" />
           <span className="text-xl text-[rgb(83,199,240)] ml-2">역이요</span>
         </div>
         <div className="flex flex-row items-center">
