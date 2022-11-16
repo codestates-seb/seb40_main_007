5630c826b35ca19ebbf6560fca421584c90f8d17
Eugenius1st <eugenius1st@gmail.com>
2022-11-16 15:39:41 +0900

    [#64]: 헤더 모바일 버전 디자인

--- client/src/Routes/mainPage.js
+++ client/src/Routes/mainPage.js
@@ -1,4 +1,4 @@
-import Header from "../components/Header";
+import LoginHeader from "../components/LoginHeader";
 import CategoryTabs from "../components/MainPage/CategoryTab";
 import MainMap from "../components/MainPage/MainMap";
 import PostList from "../components/MainPage/Posts/PostList";
@@ -9,7 +9,7 @@ import WriteModal from "../components/WriteModal";
 const MainPage = () => {
   return (
     <>
-      <Header />
+      <LoginHeader />
       <div className="flex flex-col items-center">
         <div className="grid grid-cols-1 lg:grid-cols-2 lg:w-[1024px]">
           <div>
