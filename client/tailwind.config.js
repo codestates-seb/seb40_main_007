7360fc4bd9bb9c982be972fc2ee86da07b6302e5
raon9401 <sion9401@gmail.com>
2022-11-18 20:27:54 +0900

    style: 로딩 화면 디자인 초안

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      // 커스텀 박스쉐도우
+      boxShadow: {
+        all: "0px 0px 5px 4px rgba(0, 0, 0, 0.3)",
+      },
       // 명령어: animate-waving
       // 기능: 아래위로 흔들리는 움직임
       // rotate : 회전
@@ -39,10 +43,32 @@ module.exports = {
           "90%": { transform: "translateX(7.5%)" },
           "100%": { transform: "translateX(4%)" },
         },
+        smallBounce: {
+          "0%": { transform: "translateY(3%)" },
+          "50%": { transform: "translateY(5%)" },
+          "100%": { transform: "translateY(3%)" },
+        },
+        bigBounce: {
+          "0%": { transform: "translateY(100%)" },
+          "50%": { transform: "translateY(0%)" },
+          "100%": { transform: "translateY(100%)" },
+        },
+        goTrain: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+        bgMove: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-72%)" },
+        },
       },
       animation: {
         waving: "wave 0.05s linear 3",
         moving: "moveTrain 1s linear infinite",
+        goTrain: "goTrain 10s linear infinite",
+        smallBounce: "smallBounce 1s linear infinite",
+        bigBounce: "bigBounce 1s linear infinite",
+        bgMove: "bgMove 5s linear infinite",
       },
     },
   },
