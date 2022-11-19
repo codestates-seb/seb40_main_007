cb69efdbf41f13435909bc5a7607cbfd8ae04981
casava840 <hen3403@gmail.com>
2022-11-19 14:12:04 +0900

    Feat: BoardPlanner.java추가

---
+++ src/main/java/codestates/main007/boardPlanner/BoardPlanner.java
@@ -0,0 +1,29 @@
+package codestates.main007.boardPlanner;
+
+import codestates.main007.board.Board;
+import codestates.main007.planner.Planner;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardPlanner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardPlannerId;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "planner_id")
+    private Planner plannerr;
+}
