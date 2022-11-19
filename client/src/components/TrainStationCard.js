e92ce592f6435911e52b61079b6a2ba797aad34f
Eugenius1st <eugenius1st@gmail.com>
2022-11-19 18:32:55 +0900

    [#97]: home 페이지 key 오류 수정

--- client/src/components/TrainStationCard.js
+++ client/src/components/TrainStationCard.js
@@ -1,213 +1,28 @@
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { trainInfo } from "../atoms/trainInfo";
 
 export default function TrainStationCard() {
+  const trainStationInfo = useRecoilValue(trainInfo);
   return (
     <div className="w-[1400px] pt-1">
       <div className="w-full grid grid-cols-4 gap-1">
-        <Link to="/main/1">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/행신역사진.png"
-              alt="행신역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              행신역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/2">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/서울역사진.png"
-              alt="서울역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              서울역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/3">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/영등포역사진.png"
-              alt="영등포역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              영등포역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/4">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/광명역사진.png"
-              alt="광명역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              광명역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/5">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/수원역사진.png"
-              alt="수원역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              수원역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/6">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/천안아산역사진.png"
-              alt="천안아산역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              천안아산역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/7">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/오송역사진.png"
-              alt="오송역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              오송역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/8">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/대전역사진.png"
-              alt="대전역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              대전역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/9">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/김천구미역사진.png"
-              alt="김천구미역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              김천구미역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/10">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/서대구역사진.png"
-              alt="서대구역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              서대구역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/11">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/동대구역사진.png"
-              alt="동대구역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              동대구역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/12">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/밀양역사진.png"
-              alt="밀양역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              밀양역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/13">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/구포역사진.png"
-              alt="구포역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              구포역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/14">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/부산역사진.png"
-              alt="부산역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)] "
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute ">
-              부산역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/15">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/울산역사진.png"
-              alt="울산역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              울산역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/16">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/신경주역사진.png"
-              alt="신경주역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute">
-              신경주역
-            </div>
-          </div>
-        </Link>
-        <Link to="/main/17">
-          <div className="relative flex justify-center items-center">
-            <img
-              src="images/기차역사진/포항역사진.png"
-              alt="포항역"
-              className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)] "
-            />
-            <div className="text-3xl text-white pt-1 font-semibold absolute ">
-              포항역
-            </div>
-          </div>
-        </Link>
+        {trainStationInfo.map((el) => {
+          return (
+            <Link to={`/main/${el.id}`} key={el.id}>
+              <div className="relative flex justify-center items-center">
+                <img
+                  src={`images/기차역사진/${el.train}사진.png`}
+                  alt={el.train}
+                  className="w-full h-full rounded-xl hover:ring hover:ring-[rgb(83,199,240)]"
+                />
+                <div className="text-3xl text-white pt-1 font-semibold absolute">
+                  {el.train}
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
