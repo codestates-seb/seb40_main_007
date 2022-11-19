d50e30420074423139b137a5ddcd66f9f5cb94da
casava840 <hen3403@gmail.com>
2022-11-19 18:21:43 +0900

    Feat: BoardPlannerRepository 추가

---
+++ src/main/java/codestates/main007/boardPlanner/BoardPlannerRepository.java
@@ -0,0 +1,6 @@
+package codestates.main007.boardPlanner;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardPlannerRepository extends JpaRepository<BoardPlanner, Long> {
+}
