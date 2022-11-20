9af7328d4324e31109c492bdae9632b33105cf4c
raon9401 <sion9401@gmail.com>
2022-11-20 23:02:57 +0900

    style: 별점수 색상 변경 및 별1개짜리 디자인

--- client/src/components/MainPage/Posts/PostStarScore.js
+++ client/src/components/MainPage/Posts/PostStarScore.js
@@ -1,4 +1,4 @@
-const PostStarScore = ({ score }) => {
+const PostStarScore = ({ score, size }) => {
   return (
     <>
       <div className="flex flex-row">
@@ -7,14 +7,14 @@ const PostStarScore = ({ score }) => {
             <span key={num}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width={size ? `${size}` : "20"}
+                height={size ? `${size}` : "20"}
                 viewBox="0 0 14 14"
               >
                 <path
                   d="M9,2l2.163,4.279L16,6.969,12.5,10.3l.826,4.7L9,12.779,4.674,15,5.5,10.3,2,6.969l4.837-.69Z"
                   transform="translate(-2 -2)"
-                  fill={score >= num ? "yellow" : "lightgray"}
+                  fill={score >= num ? "#FFD203" : "lightgray"}
                 />
               </svg>
             </span>
---
+++ client/src/components/OneStarScore.js
@@ -0,0 +1,25 @@
+const OneStarScore = ({ score, size }) => {
+  return (
+    <>
+      <div className="flex flex-row items-center justify-center">
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={size ? `${size}` : "20"}
+            height={size ? `${size}` : "20"}
+            viewBox="0 0 14 14"
+          >
+            <path
+              d="M9,2l2.163,4.279L16,6.969,12.5,10.3l.826,4.7L9,12.779,4.674,15,5.5,10.3,2,6.969l4.837-.69Z"
+              transform="translate(-2 -2)"
+              fill={"#FFD203"}
+            />
+          </svg>
+        </span>
+        <p className="text-base pl-0.5 font-semibold">{score}</p>
+      </div>
+    </>
+  );
+};
+
+export default OneStarScore;
