a8760f6e224130c248cac720e6377a10d967576d
raon9401 <sion9401@gmail.com>
2022-11-21 02:04:34 +0900

    [#98]style: 내 여행 계획 카카오지도 추가 및 게시글 리스트와 연결

--- client/src/Routes/myPages/myTravelPage.js
+++ client/src/Routes/myPages/myTravelPage.js
@@ -1,16 +1,24 @@
 import Header from "../../components/Header";
 import MyPageTab from "../../components/MyPageTab";
 import MyTravelList from "../../components/mytravel/MyTravelList";
+import { postDummyState } from "../../atoms/dummyData";
+import { useRecoilValue } from "recoil";
+import MyTravelMap from "../../components/mytravel/MyTravelMap";
 
 const MyTravelPage = () => {
+  const [...posts] = useRecoilValue(postDummyState);
+
+  const dummyData = posts.slice(0, 10);
+
   return (
     <>
       <Header />
       <div className="mypage-header-tab">
         <MyPageTab index={"내역이요"} />
-        <div className="w-full max-w-xl flex justify-center items-center flex-col">
-          <div className="w-full flex justify-end">
-            <MyTravelList></MyTravelList>
+        <div className="lg:w-full w-full flex justify-center items-center flex-col">
+          <div className="w-full max-w-7xl flex flex-col sm:flex-row justify-end gap-2 ">
+            <MyTravelMap data={dummyData} />
+            <MyTravelList data={dummyData} />
           </div>
         </div>
       </div>
