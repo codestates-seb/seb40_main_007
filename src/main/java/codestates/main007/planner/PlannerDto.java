51cea092648c8c6f2f3be3e3ea2b90e004b2da11
casava840 <hen3403@gmail.com>
2022-11-21 13:41:21 +0900

    Feat: PlannerDto추가

---
+++ src/main/java/codestates/main007/planner/PlannerDto.java
@@ -0,0 +1,57 @@
+package codestates.main007.planner;
+
+import codestates.main007.tag.TagDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class PlannerDto {
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Post {
+        private Double star;
+        private Double latitude;
+        private Double longitude;
+        private Long stationId;
+        private Long categoryId;
+        private String address;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Patch {
+        private String plannerName;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyPlannerResponse {
+        private long plannerId;
+        private String plannerName;
+        private List<PlannerDto.Board> boards;
+        private List<Integer> timeBetweenBoards;
+    }
+
+    public static class Board{
+        private long boardId;
+        private String title;
+        private String review;
+        private double star;
+        private String thumbnail;
+        private long stationId;
+        private long categoryId;
+        private String address;
+        private int upScore;
+        private int downScore;
+        private TagDto.Response tags;
+    }
+}
