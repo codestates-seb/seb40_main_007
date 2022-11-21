dba79286e96868205c4e8e92114edbedcd353f5a
casava840 <hen3403@gmail.com>
2022-11-22 02:24:31 +0900

    Feat: BoardPlannerMapper Mapmapper추가

--- src/main/java/codestates/main007/boardPlanner/BoardPlannerMapper.java
+++ src/main/java/codestates/main007/boardPlanner/BoardPlannerMapper.java
@@ -2,6 +2,12 @@ package codestates.main007.boardPlanner;
 
 import org.mapstruct.Mapper;
 
+import java.util.Map;
+
 @Mapper(componentModel = "spring")
 public interface BoardPlannerMapper {
+    default Map<Long,Integer> DtoToMap(BoardPlannerDto.PriorityPatch priorityPatchDto){
+        Map<Long,Integer> map = priorityPatchDto.getPriorities();
+        return map;
+    }
 }
