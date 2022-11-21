a67e3aab4b03163ed6cdb30e1e897f775099dea3
casava840 <hen3403@gmail.com>
2022-11-21 13:37:31 +0900

    Feat: BoardPlannerController-patchPriority추가

--- src/main/java/codestates/main007/boardPlanner/BoardPlannerController.java
+++ src/main/java/codestates/main007/boardPlanner/BoardPlannerController.java
@@ -1,6 +1,6 @@
 package codestates.main007.boardPlanner;
 
-import codestates.main007.board.BoardDto;
+import codestates.main007.planner.PlannerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +14,17 @@ public class BoardPlannerController {
     @PostMapping("/boards/{board-id}/{planner-id}")
     @ResponseStatus(HttpStatus.CREATED)
     public void postBoardPlanner(@RequestHeader(name = "Authorization") String accessToken,
-                          @RequestPart("data") BoardDto.Input postDto,
                           @RequestParam("board-id") long boardId,
                           @RequestParam("planner-id") long plannerId) throws IOException {
 
         boardPlannerService.save(accessToken, boardId, plannerId);
+    }
+
+    @PatchMapping("/{planner-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PlannerDto.MyPlannerResponse patchPriority(@RequestHeader(name = "Authorization") String accessToken,
+                                                      @PathVariable("planner-id") long plannerId,
+                                                      @RequestBody BoardPlannerDto.PriorityPatch priorityPatchDto) {
+        return boardPlannerService.updatePriority(accessToken, plannerId, priorityPatchDto);
     }
 }
