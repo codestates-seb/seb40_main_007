8ab115dd0fbe7ebaf8e849e390bf1d81ddc9cf6c
raon9401 <sion9401@gmail.com>
2022-11-22 10:55:56 +0900

    style: 스크롤 디자인

--- client/src/components/MainPage/Posts/PostList.js
+++ client/src/components/MainPage/Posts/PostList.js
@@ -7,7 +7,7 @@ function PostList() {
   const [...posts] = useRecoilValue(postDummyState);
   return (
     <div className="w-full flex flex-col">
-      <div className="mx-auto p-2 overflow-y-scroll h-[600px] scrollbar-hide">
+      <div className="mx-auto p-2 overflow-y-scroll h-[600px]">
         <div className="grid grid-cols-3 gap-y-4 gap-x-2">
           {posts.map((post) => (
             <Post key={post.boardId} data={post} />
