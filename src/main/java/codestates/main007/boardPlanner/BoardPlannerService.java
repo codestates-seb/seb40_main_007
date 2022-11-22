37d914efc53e369bb60110d8c83279991de44bcc
casava840 <hen3403@gmail.com>
2022-11-22 17:47:13 +0900

    Feat: BoardPlannerService 추가

--- src/main/java/codestates/main007/boardPlanner/BoardPlannerService.java
+++ src/main/java/codestates/main007/boardPlanner/BoardPlannerService.java
@@ -1,6 +1,7 @@
 package codestates.main007.boardPlanner;
 
 import codestates.main007.board.Board;
+import codestates.main007.board.BoardMapper;
 import codestates.main007.board.BoardService;
 import codestates.main007.exception.BusinessLogicException;
 import codestates.main007.exception.ExceptionCode;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +30,7 @@ public class BoardPlannerService {
     private final BoardPlannerRepository boardPlannerRepository;
     private final BoardPlannerMapper boardPlannerMapper;
     private final PlannerMapper plannerMapper;
+    private final BoardMapper boardMapper;
 
     public void save(String accessToken, long boardId, long plannerId) {
         if (memberService.findByAccessToken(accessToken).equals(plannerService.find(plannerId).getMember())) {
@@ -39,6 +42,23 @@ public class BoardPlannerService {
         } else throw new BusinessLogicException(ExceptionCode.MEMBER_UNAUTHORIZED);
     }
 
+    public PlannerDto.MyPlannerResponse updateTempPriority(String accessToken,
+                                                           long plannerId,
+                                                           BoardPlannerDto.PriorityPatch priorityPatchDto) {
+        List<Long> boardPlannerIds = plannerService.find(plannerId).getBoardPlanners().stream()
+                .map(BoardPlanner::getBoardPlannerId)
+                .collect(Collectors.toList());
+        if (memberService.findByAccessToken(accessToken).equals(plannerService.find(plannerId).getMember())) {
+            Map<Long, Integer> priorities = boardPlannerMapper.DtoToMap(priorityPatchDto);
+            Planner planner = plannerService.find(plannerId);
+            List<BoardPlanner> boardPlanners = plannerService.find(plannerId).getBoardPlanners();
+            for (BoardPlanner boardPlanner : boardPlanners) {
+                boardPlanner.setPriority(priorities.get(boardPlanner.getBoardPlannerId()));
+            }
+            return getMyPlannerResponse(plannerId, planner, boardPlanners);
+        } else throw new BusinessLogicException(ExceptionCode.MEMBER_UNAUTHORIZED);
+    }
+
     public PlannerDto.MyPlannerResponse updatePriority(String accessToken,
                                                        long plannerId,
                                                        BoardPlannerDto.PriorityPatch priorityPatchDto) {
@@ -46,39 +66,44 @@ public class BoardPlannerService {
                 .map(BoardPlanner::getBoardPlannerId)
                 .collect(Collectors.toList());
         if (memberService.findByAccessToken(accessToken).equals(plannerService.find(plannerId).getMember())) {
-            Map<Long, Integer> priorities = (Map<Long, Integer>) priorityPatchDto;
+            Map<Long, Integer> priorities = boardPlannerMapper.DtoToMap(priorityPatchDto);
             for (long boardPlannerId : boardPlannerIds) {
                 BoardPlanner boardPlanner = find(boardPlannerId);
                 boardPlanner.setPriority(priorities.get(boardPlannerId));
                 boardPlannerRepository.save(boardPlanner);
             }
             Planner planner = plannerService.find(plannerId);
-            List<BoardPlanner> boardPlanner = plannerService.find(plannerId).getBoardPlanners();
-            PlannerDto.MyPlannerResponse myPlannerResponseDto = PlannerDto.MyPlannerResponse.builder()
-                    .plannerId(plannerId)
-                    .plannerName(planner.getPlannerName())
-                    .boards(
-                            boardPlanner.stream()
-                                    .sorted(Comparator.comparing(BoardPlanner::getPriority))
-                                    .map(boardPlanner1 -> {
-                                        PlannerDto.Board boardDto = plannerMapper.entityToResponseDto(
-                                                boardPlanner1.getBoard());
-                                        return boardDto;
-                                    }).collect(Collectors.toList())
-                    )
-                    .timeBetweenBoards(
-                            plannerService.getTimeBetweenBoardsList(
-                                    boardPlanner.stream()
-                                            .sorted(Comparator.comparing(BoardPlanner::getPriority))
-                                            .map(boardPlanner1 -> {
-                                                Board board = boardPlanner1.getBoard();
-                                                return board;
-                                            })
-                                            .collect(Collectors.toList())
-                            )
-                    ).build();
-            return myPlannerResponseDto;
+            List<BoardPlanner> boardPlanners = plannerService.find(plannerId).getBoardPlanners();
+            return getMyPlannerResponse(plannerId, planner, boardPlanners);
         } else throw new BusinessLogicException(ExceptionCode.MEMBER_UNAUTHORIZED);
+    }
+
+    private PlannerDto.MyPlannerResponse getMyPlannerResponse(long plannerId, Planner planner, List<BoardPlanner> boardPlanners) {
+        PlannerDto.MyPlannerResponse myPlannerResponseDto = PlannerDto.MyPlannerResponse.builder()
+                .plannerId(plannerId)
+                .plannerName(planner.getPlannerName())
+                .boards(boardMapper.boardsToBoardsResponse(planner.getBoardPlanners().stream()
+                                .sorted(Comparator.comparing(BoardPlanner::getPriority))
+                        .map(BoardPlanner::getBoard)
+                        .collect(Collectors.toList()))
+                )
+                .timeBetweenBoards(
+                        plannerService.getTimeBetweenBoardsList(
+                                boardPlanners.stream()
+                                        .sorted(Comparator.comparing(BoardPlanner::getPriority))
+                                        .map(boardPlanner -> {
+                                            Board board = boardPlanner.getBoard();
+                                            return board;
+                                        })
+                                        .collect(Collectors.toList())
+                        )
+                ).build();
+        return myPlannerResponseDto;
+    }
+
+    public void deleteBoardPlanner(String accessToken, long boardPlannerId) throws IOException {
+        BoardPlanner boardPlanner = find(boardPlannerId);
+        boardPlannerRepository.delete(boardPlanner);
     }
 
     public BoardPlanner find(long boardPlannerId) {
