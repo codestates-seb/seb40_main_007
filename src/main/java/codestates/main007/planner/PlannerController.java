176a5dc14ae72758db3f48ab661d053398b00e3f
casava840 <hen3403@gmail.com>
2022-11-23 19:27:09 +0900

    Feat: 모달용 getPlanners추가

--- src/main/java/codestates/main007/planner/PlannerController.java
+++ src/main/java/codestates/main007/planner/PlannerController.java
@@ -1,5 +1,6 @@
 package codestates.main007.planner;
 
+import codestates.main007.dto.MultiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,36 +12,45 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class PlannerController {
     private final PlannerService plannerService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void postPlanner(
             @RequestHeader(name = "Authorization") String accessToken,
             @RequestBody PlannerDto.Input inputDto) throws IOException {
 
-        plannerService.save(accessToken,inputDto);
+        plannerService.save(accessToken, inputDto);
     }
 
     @PatchMapping("/{planner-id}")//일단 이름만 바꿀 수 있음.
     @ResponseStatus(HttpStatus.OK)
     public void patchPlanner(@RequestHeader(name = "Authorization") String accessToken,
-                           @PathVariable("planner-id") long plannerId,
-                           @RequestBody PlannerDto.Input patchDto) {
+                             @PathVariable("planner-id") long plannerId,
+                             @RequestBody PlannerDto.Input patchDto) {
 
         plannerService.update(accessToken, plannerId, patchDto);
     }
 
     @GetMapping("/{planner-id}")
     @ResponseStatus(HttpStatus.OK)
-    public PlannerDto.MyPlannerResponse getMyPlannerPage (@RequestHeader(name = "Authorization") String accessToken,
-                                            @PathVariable("planner-id") long plannerId) {
+    public PlannerDto.MyPlannerResponse getMyPlannerPage(@RequestHeader(name = "Authorization") String accessToken,
+                                                         @PathVariable("planner-id") long plannerId) throws InterruptedException {
 
-        return plannerService.getMyPlannerPage(accessToken,plannerId);
+        return plannerService.getMyPlannerPage(accessToken, plannerId);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public MultiResponseDto<PlannerDto.MyPlannersResponse>
+    getMyPlanners(@RequestHeader(name = "Authorization") String accessToken) {
+
+        return MultiResponseDto.of(plannerService.getMyPlanners(accessToken));
     }
 
     @DeleteMapping("/{planner-id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePlanner(@RequestHeader(name = "Authorization") String accessToken,
-                            @PathVariable("planner-id") long plannerId) throws IOException {
+                              @PathVariable("planner-id") long plannerId) throws IOException {
 
         plannerService.deletePlanner(accessToken, plannerId);
     }
