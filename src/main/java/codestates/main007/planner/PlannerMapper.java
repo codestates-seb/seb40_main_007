0f149564f1b6fac5b92b44facc39f6efaaffaffc
casava840 <hen3403@gmail.com>
2022-11-23 19:27:43 +0900

    Feat: List용 mapper추가

--- src/main/java/codestates/main007/planner/PlannerMapper.java
+++ src/main/java/codestates/main007/planner/PlannerMapper.java
@@ -2,7 +2,10 @@ package codestates.main007.planner;
 
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface PlannerMapper {
     Planner inputDtoToentity(PlannerDto.Input inputDto);
+    List<PlannerDto.MyPlannersResponse> entityListToResponseDtoList(List<Planner> planners);
 }
