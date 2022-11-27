dc69e8ad2082a7976beab1b7e495bd862205180a
woong-sung <cjswo138@gmail.com>
2022-11-27 23:04:50 +0900

    refactor: email 내용 변경

--- src/main/java/codestates/main007/service/EmailService.java
+++ src/main/java/codestates/main007/service/EmailService.java
@@ -22,9 +22,9 @@ public class EmailService {
         message.addRecipients(MimeMessage.RecipientType.TO, address);
         message.setFrom(FROM_ADDRESS);
         message.setSubject("안녕하세요 역이요입니다");
-        message.setText("<div align='center' style='margin:100px; font-family: Malgun Gothic;>\n" +
-                        "<div style='background-color:white; opacity:0.7; border-bottom-left-radius: 3em; border-bottom-right-radius: 3em;'>\n" +
-                        "<br>" +
+        message.setText("<div align='center' style='margin:100px; font-family: Malgun Gothic;'>\n" +
+                        "<div style='background-color:white; opacity:0.7;'>\n" +
+                        "<br>\n" +
                         "<h1>안녕하세요<br>당신의 여행파트너! 역이요입니다</h1>\n" +
                         "\n" +
                         "<h2>역이요와 함께 여행해주셔서 감사합니다!<h2>\n" +
@@ -32,11 +32,13 @@ public class EmailService {
                         "</div>\n" +
                         "<br>\n" +
                         "<img src ='https://pre-032-bucket.s3.ap-northeast-2.amazonaws.com/Logo.png' width='200' height='200' align='center'>\n" +
-                        "<div style='margin:100px; border:1px solid black; font-family:verdana; background-color:#ccffff; opacity: 0.8; border-radius: 2em;';>\n" +
-                        "<p>귀하의 임시 비밀번호는 <strong>"+password+"</strong> 입니다.</p>\n" +
+                        "<br><br>\n" +
+                        "<br>\n" +
+                        "<p>귀하의 임시 비밀번호는 아래와 같습니다.</p>\n" +
+                        "<p><strong>\"+password+\"</strong></p>\n" +
                         "<p>반드시 비밀번호를 변경해주세요!</p>\n" +
-                        "</div>\n" +
-                        "<p>홈페이지로 이동 : <a href='http://ec2-43-201-80-20.ap-northeast-2.compute.amazonaws.com:8080'>역이요 홈페이지 바로가기</a></p>"
+                        "<br>\n" +
+                        "<p style ='font-size:10'>홈페이지로 이동 : <a href='http://ec2-43-201-80-20.ap-northeast-2.compute.amazonaws.com:8080'>역이요 홈페이지로 이동</a></p>"
                 ,"utf-8","html");
         //todo : 배포링크로 변경
         mailSender.send(message);
