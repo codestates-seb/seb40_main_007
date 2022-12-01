b3d87a1ee9b29e3f734600e9ef47ea8626c2977d
woong-sung <cjswo138@gmail.com>
2022-12-01 10:34:37 +0900

    refactor: 리다이렉트 배포uri로 수정

--- src/main/java/codestates/main007/auth/handler/OAuthMemberAuthenticationSuccessHandler.java
+++ src/main/java/codestates/main007/auth/handler/OAuthMemberAuthenticationSuccessHandler.java
@@ -133,9 +133,9 @@ public class OAuthMemberAuthenticationSuccessHandler extends SimpleUrlAuthentica
         return UriComponentsBuilder
                 .newInstance()
                 .scheme("http")
-                .host("localhost")
+                .host("http://pre-032-bucket.s3-website.ap-northeast-2.amazonaws.com")
                 //todo : 테스트시 3000 배포시 ec2주소
-                .port(3000)
+//                .port(3000)
                 .path("callback/receive-token.html")
                 .queryParams(queryParams)
                 .build()
