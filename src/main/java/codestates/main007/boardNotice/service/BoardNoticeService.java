9e656534b3c71604fc05e4ff5bb117e79a432f82
woong-sung <cjswo138@gmail.com>
2022-12-03 20:17:04 +0900

    feat: BoardNoticeService save 메서드 작성

--- src/main/java/codestates/main007/boardNotice/service/BoardNoticeService.java
+++ src/main/java/codestates/main007/boardNotice/service/BoardNoticeService.java
@@ -1,5 +1,11 @@
 package codestates.main007.boardNotice.service;
 
+import codestates.main007.board.entity.Board;
+import codestates.main007.board.service.BoardService;
+import codestates.main007.boardNotice.entity.BoardNotice;
+import codestates.main007.boardNotice.repository.BoardNoticeRepository;
+import codestates.main007.member.entity.Member;
+import codestates.main007.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +15,19 @@ import javax.transaction.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class BoardNoticeService {
-    public void save(){
+    private final BoardService boardService;
+    private final MemberService memberService;
+    private final BoardNoticeRepository boardNoticeRepository;
+    public void save(long boardId, String accessToken, String notice){
+        Board board = boardService.find(boardId);
+        Member sender = memberService.findByAccessToken(accessToken);
 
+        BoardNotice boardNotice = BoardNotice.builder()
+                .board(board)
+                .sender(sender)
+                .notice(notice)
+                .build();
+
+        boardNoticeRepository.save(boardNotice);
     }
 }
